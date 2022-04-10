935cf421037ef9c9ee2af750075c88d01bbc0657
JZujewski1 <juliusz.zujewski@gmail.com>
2022-04-10 10:55:47 +0200

    Typescript type additon

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const apiUrl = "https://jsonplaceholder.typicode.com";
+
+const postsUrl: string = apiUrl + "/posts";
+const commentsUrl: string = `${apiUrl}/comments`;
+const usersUrl: string = `${apiUrl}/users`;
